4a8fef276052416d8c335b51b205ee9135fcdd7b
miroslavasenaj <43532622+miroslavasenaj@users.noreply.github.com>
2019-01-10 02:06:15 +0100

    Add files via upload

---
+++ Miroslava_Senajova Portfolio/script/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const vueApp = new Vue ({
+el:'#app',
+data:{
+  apelCaption: 'I worked on this project with project group. Our task was redesign outdated website in one month. My role was UX design and front-end development',
+  zeroCaption: 'With this project me and my group tried to solve wicked problem - Environmental threats. We came up with prototype for a application to help reduce waste production of an individual.',
+  infoweb: 'This is the second solution for wicked problem we chose, we created an design for a informative website.'
+
+}
+
+});
